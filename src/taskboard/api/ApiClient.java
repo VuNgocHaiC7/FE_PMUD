61df06d86697144636c7dffff99ac0621a45c2b0
VungochaiC7 <pro9zuka17@gmail.com>
2025-12-19 21:10:37 +0700

    update api

--- src/taskboard/api/ApiClient.java
+++ src/taskboard/api/ApiClient.java
@@ -9,8 +9,8 @@ import java.time.Duration;
 
 public class ApiClient {
     // >>> CẬP NHẬT ĐỊA CHỈ SERVER MỚI <<<
-    // Lưu ý: Tôi lấy phần gốc là ".../tms/api", các phần sau sẽ là endpoint
-    private static final String BASE_URL = "http://localhost:8080/api";
+    // Lưu ý:lấy phần gốc là ".../tms/api", các phần sau sẽ là endpoint
+    private static final String BASE_URL = "https://miki-untactical-instrumentally.ngrok-free.dev/api";
     
     // ... (Phần code còn lại giữ nguyên không đổi) ...
     private static final HttpClient client = HttpClient.newBuilder()
